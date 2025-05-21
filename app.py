215751527b2d0aec0591f1a05fe3ec98cecadfa9
Mikelubuntu <mikel.hernandez@casaricardo.com>
2025-05-21 15:17:09 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -880,5 +880,7 @@ def inventario_historico():
         })
     return render_template('inventario_historico.html', historicos=historicos_lista)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Railway te asigna el puerto por variable de entorno
+    app.run(host="0.0.0.0", port=port)
